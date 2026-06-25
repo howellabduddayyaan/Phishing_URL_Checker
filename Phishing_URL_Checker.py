cb693cc030f7d5ccab5a591315300e18ab766d72
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-06-25 22:13:07 +0200

    Improvements : Safe URL's added for testing

--- Phishing_URL_Checker.py
+++ Phishing_URL_Checker.py
@@ -2,52 +2,58 @@
 # === Phishing URL Checker ===
 # ============================
 
-import re
 from urllib.parse import urlparse
 
 Suspicious_Keywords = [
-                    "login", 
-                    "verify",
-                    "secure", 
-                    "account", 
-                    "update",
-                    "bank", 
-                    "password", 
-                    "confirm"
+                    "login", "verify","secure", "account", 
+                    "update","bank", "password", "confirm"
                     ]
 
 # _________________________________________________________________________________________________
 
 def is_ip(address):
-    return re.match(r"^\d{1,3}(\.\d{1,3}){3}$", address) is not None
+    parts = address.split(".")
+    
+    if len(parts) != 4:
+        return False
+
+    for part in parts:
+        if not part.isdigit():
+            return False
+
+        if not 0 <= int(part) <= 255:
+            return False
+
+    return True
+
+# _________________________________________________________________________________________________
 
 def analyze_url(url):
     print(f"\nAnalyzing: {url}")
 
     score = 0
     parsed = urlparse(url)
-
     domain = parsed.netloc.lower()
     path = parsed.path.lower()
 
 # _________________________________________________________________________________________________
 
     if is_ip(domain) :
-        print("[!] Uses IP adress instead of domain")
+        print("[!] Uses IP address instead of domain")
         score += 2
-        
+
 # _________________________________________________________________________________________________
 
-    if "@" in url :
+    if "@" in parsed.geturl() :
         print("[!] Contains '@' (Possible redirect trick)")
         score += 1
-        
+
 # _________________________________________________________________________________________________
 
     if len(url) > 75 :
-        print("[!] URL is unsually long")
+        print("[!] URL is unusually long")
         score += 1
-        
+    
 # _________________________________________________________________________________________________
 
     for keyword in Suspicious_Keywords:
@@ -63,13 +69,25 @@ def analyze_url(url):
         
 # _________________________________________________________________________________________________
 
+    if parsed.scheme != "https":
+        print("[!] Not using HTTPS")
+        score += 1
+        
+# _________________________________________________________________________________________________
+
+    if "-" in domain:
+        print("[!] Domain contains hyphen")
+        score += 1
+
+# _________________________________________________________________________________________________
+
     print("\nRisk Score : ", score)
 
     if score >= 4 :
         print("!!! HIGH RISK : Possible Phishing URL")
         
     elif score >= 2 :
-        print("!! MEDIUM RISK : Proceed this URL with caution")
+        print("!! MEDIUM RISK : Proceed with caution")
         
     else:
         print("! LOW RISK : No threats within the URL detected")
